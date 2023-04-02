ae868e63322139a413587c51329736a3fffc3655
Shahd <shahdlubb@gmail.com>
2023-04-03 00:46:00 +0300

    Query Syntax 1 excersise is done

--- QuerySyntax1/QuerySyntax1.cs
+++ QuerySyntax1/QuerySyntax1.cs
@@ -7,13 +7,10 @@ namespace QuerySyntax1
     {
         public static IEnumerable<string> FilterAndSort(IEnumerable<string> inValues, string pattern)
         {
-            return from value in inValues
-                //// START SOLUTION ////
-                where value.Contains(pattern)
-                orderby value
-                //// END SOLUTION ////
-                // LINQ instructions here
-                select value;
+           return from value in inValues
+                  where value.Contains(pattern)
+                  orderby value
+                  select value;
         }
     }
 }
