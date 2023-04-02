a8a2cdd0391032d6268196ba04835e888ac5af2f
Shahd <shahdlubb@gmail.com>
2023-04-03 00:28:29 +0300

    Method Syntax excersise is done

--- MethodSyntax1/MethodSyntax1.cs
+++ MethodSyntax1/MethodSyntax1.cs
@@ -8,12 +8,10 @@ namespace MethodSyntax1
         public static IEnumerable<string> FilterAndSort(IEnumerable<string> inValues, string pattern)
         {
             return inValues
-                //// START SOLUTION ////
-                .Where(_ => _.Contains(pattern))
-                .OrderBy(_ => _)
-                //// END SOLUTION ////
-                // LINQ method calls here
-                ;
+                  .Where( str => str.Contains(pattern))
+                  .OrderBy(str => str)
+                  .ToList();
+           
         }
     }
 }
